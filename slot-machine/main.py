7a835571f497869f76fcaa5363af7783c657193c
Ben Aderaw <asyncba@gmail.com>
2023-12-22 13:32:04 -0500

    implemented deposit function > asks for user input on huw much they want to deposit > returs the deposit amount

--- slot-machine/main.py
+++ slot-machine/main.py
@@ -1,1 +1,21 @@
-print('Hello World')
+
+# collect user deposit
+def deposit():
+    while True:
+        amount = input('What would you like to deposit? $')
+        if amount.isdigit():
+            amount = int(amount)
+            if amount > 0:
+                break
+            else: 
+                print('Amount must be greater then 0.')
+
+        else:
+            print('Please enter a number.')
+
+    return amount
+
+def main():
+    balance = deposit()
+
+main()
