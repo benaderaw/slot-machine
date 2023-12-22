a453a71f10b503ff574e1ac62cf526a2724d70d5
Ben Aderaw <asyncba@gmail.com>
2023-12-22 13:52:50 -0500

    implemented user input on the number of lines user wantd to bet on

--- slot-machine/main.py
+++ slot-machine/main.py
@@ -1,3 +1,4 @@
+MAX_LINES = 3
 
 # collect user deposit
 def deposit():
@@ -15,7 +16,28 @@ def deposit():
 
     return amount
 
+# get the number of lines user wants to be on
+def get_number_of_line():
+    while True:
+        lines = input(f'Enter the number of lines to bet on (1-{str(MAX_LINES)}). ')
+        if lines.isdigit():
+            lines = int(lines)
+            if 1 <= lines <= MAX_LINES:
+                break
+            else: 
+                print('Enter a valid number of lines.')
+
+        else:
+            print('Please enter a number.')
+    
+    return lines
+
+
+
 def main():
     balance = deposit()
+    bet_lines = get_number_of_line()
+
+    print(balance, bet_lines)
 
 main()
